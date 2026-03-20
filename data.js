ee842dd80949f373bb390ff10abea0b3c8d2b6f0
Sebastião Sukuakueche <sukuakueches@gmail.com>
2026-03-20 16:48:33 +0100

    feat: improve translations and added language switcher

--- data.js
+++ data.js
@@ -5,7 +5,8 @@ export const translations = {
         'website-under-development': 'Site está em desenvolvimento',
         'cci': 'Câmara de Comércio e Indústria',
         'in-production': 'Em produção',
-        'building-something-remarkable': 'A construir algo grande'
+        'building-something-remarkable': 'A construir algo grande',
+        'language': 'Idioma',
     }, 
     en: {
         'app-title': 'Chamber of Commerce and Industry - Angola & Botswana',
@@ -13,6 +14,7 @@ export const translations = {
         'website-under-development': 'Website is under development',
         'cci': 'Chamber of Commerce and Industry',
         'in-production': 'In Production',
-        'building-something-remarkable': 'Building something remarkable'
+        'building-something-remarkable': 'Building something remarkable',
+        'language': 'Language',
     }
 }
